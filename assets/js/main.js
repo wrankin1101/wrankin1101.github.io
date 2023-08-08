b4575627cc55029f7726be4cf335be9332609413
Will Rankin <willrankin@Wills-MacBook-Pro.local>
2023-08-08 13:33:43 +0900

    adding form functionality, starting implementing portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,7 +104,7 @@
 					overlayOpacity: 0.85,
 					popupCloserText: '',
 					popupLoaderText: '',
-					selector: '.work-item a.image',
+					selector: '',//'.work-item a.image',
 					usePopupCaption: true,
 					usePopupDefaultStyling: false,
 					usePopupEasyClose: false,
